cd89d1b43e75ba97f7ad82b23c75bde77ca552c4
Anxten <allanjeremyb@gmail.com>
2026-02-19 00:14:16 +0700

    Feat: Setup database connection and initialization logic

---
+++ src/data/database.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from src.data.models import Base
+
+load_dotenv()
+
+# Ambil config dari .env
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Setup Engine
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+    """Fungsi sakti untuk membuat semua tabel"""
+    print(f"Connecting to database at {DB_HOST}...")
+    Base.metadata.create_all(bind=engine)
+    print("✅ Success! Tables created successfully.")
